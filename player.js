8926990d15e7897a4ee22bd57ae38135a4ddd8c6
Bhimneopane <bhimneopane1212@gmail.com>
2025-04-11 01:06:12 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,11 +1,16 @@
 function fetchTicket() {
     let code = document.getElementById('playerCode').value;
-    let gameStartTime = document.getElementById('gameStartTime').innerText;
-    let currentTime = new Date().getHours();
+    let gameStartTime = localStorage.getItem("gameStartTime");
+    let currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
-    if (gameStartTime > currentTime) {
-        alert("Game has not started yet!");
+    if (gameStartTime && currentTime < gameStartTime) {
+        alert(`Game has not started yet! It will start at ${gameStartTime}`);
     } else {
-        document.getElementById('playerTicket').innerHTML = `<h3>Your Ticket: ${code}</h3>`;
+        let tickets = JSON.parse(localStorage.getItem("ticketCodes"));
+        if (tickets[code]) {
+            document.getElementById('playerTicket').innerHTML = displayTicketGrid(tickets[code]);
+        } else {
+            alert("Invalid code!");
+        }
     }
 }
